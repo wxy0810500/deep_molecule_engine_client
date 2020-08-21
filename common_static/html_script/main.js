4467b252f907d2608c0eff08e3af155c0bac3434
wxy0810500 <newman_wang@sina.com>
2020-08-21 10:04:57 +0800

    make it available in js:checkAllInput() at the page /advancedSearch

--- common_static/html_script/main.js
+++ common_static/html_script/main.js
@@ -73,20 +73,8 @@ class Message {
     }
 }
 
-function checkAllInput() {
-    // checkbox
-    const container = document.querySelector("#id_modelTypes");
-    const checkboxGroup = container.querySelectorAll("input[type='checkbox']");
-    let selectAtLeastOne = false
-    for (let i = 0; i < checkboxGroup.length; i++) {
-        if (checkboxGroup[i].checked === true) {
-            selectAtLeastOne = true
-        }
-    }
-    if (!selectAtLeastOne) {
-        alert('Please, select at least one AI model type!')
-        return false
-    }
+
+function checkInputStringAndFile() {
     // check textarea, inputFile
     const textarea = document.querySelector("#id_inputStr");
     const uploadInputFile = document.querySelector("#id_uploadInputFile")
@@ -99,5 +87,25 @@ function checkAllInput() {
         });
         return false
     }
+    return true
+}
 
+
+function checkAllInput() {
+    // checkbox
+    const container = document.querySelector("#id_modelTypes");
+    if (container !== null) {
+        const checkboxGroup = container.querySelectorAll("input[type='checkbox']");
+        let selectAtLeastOne = false
+        for (let i = 0; i < checkboxGroup.length; i++) {
+            if (checkboxGroup[i].checked === true) {
+                selectAtLeastOne = true
+            }
+        }
+        if (!selectAtLeastOne) {
+            alert('Please, select at least one AI model type!')
+            return false
+        }
+    }
+    return checkInputStringAndFile()
 }
