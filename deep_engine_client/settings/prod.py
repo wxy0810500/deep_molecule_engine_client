5ea24090d34c7def7d61a6a4c265bfff707c080a
wxy0810500 <newman_wang@sina.com>
2020-11-09 18:07:40 +0800

    update STATIC_ROOT on ENV:prod and modify STATIC_URL to '/admet/static'

--- deep_engine_client/settings/prod.py
+++ deep_engine_client/settings/prod.py
@@ -13,15 +13,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 from .base import BASE_DIR
 import os
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/static/'
 
 # 当运行 python manage.py collectstatic 的时候
 # STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
 # 把这些文件放到一起是为了用apache等部署的时候更方便
-PROD_STATIC_DIR = "/home/ghddirun/admet/static"
-STATIC_ROOT = PROD_STATIC_DIR
+STATIC_ROOT = f"('SERVER_DEVELOPMENT_DIR')/static/"
 
 # DEBUG = True
 DEBUG = False
