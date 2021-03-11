b533c100e8ac5fdc85f382de58567e803e71abe8
wxy0810500 <newman_wang@sina.com>
2021-03-11 16:14:37 +0800

    update prediction server host

--- deep_engine_client/settings/settings.py
+++ deep_engine_client/settings/settings.py
@@ -16,6 +16,10 @@ print(f"DME_EMV : {env}")
 if env == 'prod':
     from .prod import *
     RUNTIME_ENV = 'prod'
+elif env == 'prod_test':
+    from .prod_test import *
+
+    RUNTIME_ENV = 'prod'
 else:
     from .local_test import *
     RUNTIME_ENV = 'test'
