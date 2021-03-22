66ca96c8fd551eb6f0abbbb9b12430839db989f4
wxy0810500 <newman_wang@sina.com>
2021-03-22 16:52:37 +0800

    add db file

--- deep_engine_client/settings/local_test.py
+++ deep_engine_client/settings/local_test.py
@@ -26,7 +26,7 @@ STATICFILES_DIRS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
