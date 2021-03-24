13f7a04acbfce3684c4d6c402a67f9e76f0cbb99
wxy0810500 <newman_wang@sina.com>
2021-03-24 15:45:34 +0800

    add placeholder in inputStr

--- deep_engine_client/forms.py
+++ deep_engine_client/forms.py
@@ -7,22 +7,26 @@ from .fields import RestrictedFileField
 class CommonInputForm(forms.Form):
     INPUT_TYPE_DRUG_NAME = 'drugName'
     INPUT_TYPE_SMILES = 'raw_smiles'
+
     OUTPUT_TYPE_FILE = 'file'
     OUTPUT_TYPE_WEB_PAGE = 'webPage'
     INPUT_NAME_STR_SEPARATOR_RX = '!|,|;|\t|\n|\r\n'
     INPUT_SMILES_STR_SEPARATOR_RX = '!|,|;|\t|\n|\r\n| '
     MAX_SMILES_LEN: int = 500
 
-    inputType = forms.ChoiceField(widget=forms.RadioSelect, label="", required=True,
-                                  choices=[(INPUT_TYPE_DRUG_NAME, 'DRUG_NAMES'), (INPUT_TYPE_SMILES, 'SMILES')],
-                                  initial=[INPUT_TYPE_SMILES], )
-    inputStr = forms.CharField(widget=forms.Textarea, label="", max_length=2000, required=False, )
+    inputType = forms.ChoiceField(widget=forms.RadioSelect(attrs={
+
+    }),
+        label="", required=True,
+        choices=[(INPUT_TYPE_DRUG_NAME, 'DRUG_NAMES'), (INPUT_TYPE_SMILES, 'SMILES')],
+        initial=[INPUT_TYPE_SMILES], )
+    inputStr = forms.CharField(widget=forms.Textarea, label="", max_length=2000, required=False)
     # error_messages={"required": "Please enter the SMILES or "
     #                             "NAMES of your compound, one per line"})
 
     uploadInputFile = RestrictedFileField(widget=forms.FileInput(attrs={
         "onchange": "document.getElementById('inputFileName').innerText=this.files[0].name"
-    }), required=False, label="", max_upload_size=2621440,)  # 2.5M
+    }), required=False, label="", max_upload_size=2621440, )  # 2.5M
 
     outputType = forms.ChoiceField(widget=forms.RadioSelect, label="", required=True,
                                    choices=[(OUTPUT_TYPE_FILE, 'Download predictive results as csv file '),
